5d8b33813a8cb65c12a02ab6dbf0ee36f4e64427
ViniTex231 <viniteixeira2828@gmail.com>
2023-07-21 11:21:33 -0300

    first

---
+++ pages/home/index.jsx
@@ -0,0 +1,11 @@
+import {View, Text, StyleSheet} from 'react-native'
+import styles from './styles'
+
+export default function Home(){
+    return(
+        <View style={styles.container}>
+            <Text>Página Home</Text>
+        </View>
+    )
+}
+
